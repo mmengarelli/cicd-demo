c776ff4c16aec4aaf5b0b87a4cb5bd836b1cbb20
michael.mengarelli@databricks.com <michael.mengarelli@databricks.com>
2022-03-21 20:51:33 +0000

    adding files

--- cicd_sample_project/notebooks/mydemo.py
+++ cicd_sample_project/notebooks/mydemo.py
@@ -12,3 +12,14 @@ job.launch()
 # COMMAND ----------
 
 print("This is working")
+
+# COMMAND ----------
+
+# MAGIC %sh pwd
+
+# COMMAND ----------
+
+You can use the command %sh pwd in a notebook inside a Repo to check if Files in Repos is enabled.
+
+If Files in Repos is not enabled, the response is /databricks/driver.
+If Files in Repos is enabled, the response is /Workspace/Repos/<path to notebook directory> .
